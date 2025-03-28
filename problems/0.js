0092e7973ceaf26fe12c720a886117a5dc2324ed
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-03-28 09:01:49 -0500

    Problem 0

--- problems/0.js
+++ problems/0.js
@@ -9,6 +9,23 @@
  * @example 92 -> "A", 75 -> "B"
 */
 function problem(percentage) {
+
+    if (percentage >= 90 && percentage <= 100) {
+        return "A";
+    }
+    else if (percentage >= 80 && percentage <= 89) {
+        return "B";
+    }
+    else if (percentage >= 70 && percentage <= 79) {
+        return "C";
+    }
+    else if (percentage >= 60 && percentage <= 69) {
+        return "D";
+    }
+    else if (percentage >= 0 && percentage <= 59) {
+        return "F";
+    }
+
     return null;
 }
 
