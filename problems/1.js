356122fff45a198bf4166a617c57d4ced0f6a9b3
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-03-28 09:05:25 -0500

    Problem 1

--- problems/1.js
+++ problems/1.js
@@ -9,12 +9,10 @@
 */
 function problem(str) {
     
-    // TODO: Convert this to an arrow function
-    // TODO: Fix the bug inside the function
-    function getLastLetter(word) {
-        return word.charAt(word.length)
+    getLastLetter = (word) => {
+        return word.charAt(word.length - 1);
     }
-    
+
     return getLastLetter(str);
 }
 
