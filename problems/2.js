45d424dd9fc7ae1953f42565eac087dfed3a145e
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-03-28 09:17:23 -0500

    Problem 2

--- problems/2.js
+++ problems/2.js
@@ -7,7 +7,8 @@
  * @example ["Chase", "Raman"] -> ["Hi Chase", "Hi Raman"]
  */
 function problem(names) {
-    return null;
+    const greetedNames = names.map(name => `Hi ${name}`);
+    return greetedNames;
 }
 
 const tests = [
